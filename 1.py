321b0300eb5e830aeefe901f75165c33e11162a3
user321 <user123@gmail.com>
2025-12-02 07:36:13 +0300

    add print "номер"

---
+++ 1.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+from flask import Flask, jsonify
+import random
+import logging
+
+# Отключаем логи Flask
+log = logging.getLogger('werkzeug')
+log.setLevel(logging.ERROR)
+
+app = Flask(__name__)
+
+@app.route('/numbers')
+def get_numbers():
+    numbers = [random.randint(1, 100) for _ in range(10)]
+    return jsonify({"numbers": numbers})
+
+if __name__ == '__main__':
+    # Запускаем без debug режима
+    app.run(host='0.0.0.0', port=5000, debug=False)
